f3bb34132a00a57f1e913e47a18d35b61077a7f4
vladimir-pool <45765313+vladimir-pool@users.noreply.github.com>
2019-06-24 23:02:54 -0500

    Proyecto cargado

---
+++ index2.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>UTCANCÚN</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+ 
+  <body>
+
+    <div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted">UT CANCÚN</h3>
+
+        <nav class="navbar navbar-light" style="background-color: white;">
+          <ul class="nav nav-justified">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Alumnos</a></li>
+            <li><a href="#">Streaming</a></li>
+            <li><a href="#">Contactanos</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Universidad tecnologica de cancún</h1>
+        <p class="lead">En la universidad tecnologica de cancún, se implento el servicio de streaming. ¡Conectate ahora!.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Iniciar</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Audio</h2>
+          
+          <p>texto </p>
+          <p><a class="btn btn-primary" href="#" role="button">Detalles &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Video</h2>
+          <p>texto </p>
+          <p><a class="btn btn-primary" href="#" role="button">Detalles &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Clases en Línea</h2>
+          <p><a class="btn btn-primary"  target="_blank" href="http://192.168.10.103:8000/admin.ogv" role="button">Directo&raquo;</a></p>
+        </div>
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; 2019 UTCANCÚN, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body> 
+</html>
